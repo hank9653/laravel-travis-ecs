4f2f773ed28a376773fc489842ac2a8090d8e53b
hank9653 <t102318120@ntut.org.tw>
2018-04-03 14:33:51 +0800

    laravel-travis-ecs test

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel travis ecs test
                 </div>
 
                 <div class="links">
